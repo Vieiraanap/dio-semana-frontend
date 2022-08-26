ce821449df2da0432c69d739ca9be50cac834ada
Ana Paula <anapaulavieira.ac@gmail.com>
2022-08-26 16:18:05 -0500

    Carrossel na galeria de imagens, ajuste na inversão de temas

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,7 +9,7 @@ const txtName = document.getElementById('txtName');
 const txtEmail = document.getElementById('txtEmail');
 const txtLevel = document.getElementById('txtLevel');
 const txtCharacter = document.getElementById('txtCharacter');
-const btnSubscribe = document.getElementById('btnSubscribe');
+const btnSubscribe = document.getElementById('btn-subscribe');
 
 // mudar tema da página
 switchTheme(switchThemeButton);
